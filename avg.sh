d875e793125093af9c963fdcaf2c3d0931b56d69
Rojjanasak61 <34545417+Rojjanasak61@users.noreply.github.com>
2023-01-21 22:51:12 +0700

    add

---
+++ avg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+execution_time=()
+for i in {1..10}
+do
+    start_time=$(date +%s.%N)
+
+    ./app.py $1
+
+    end_time=$(date +%s.%N)
+    run_time=$(echo "$end_time - $start_time" | bc)
+    execution_time+=($run_time)
+done
+
+average_time=$(echo "${execution_time[*]}" | tr ' ' '\n' | awk '{s+=$1} END {print s/NR}')
+echo "Average execution time: $average_time seconds"
