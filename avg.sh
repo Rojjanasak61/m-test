2907944d9c981fde1596310f4d0c26841bd79f10
Rojjanasak61 <34545417+Rojjanasak61@users.noreply.github.com>
2023-01-24 18:28:57 +0700

    add

--- avg.sh
+++ avg.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-for i in {1..10}
-do
-    start_time=$(date +%s.%N)
+start_time=$(date +%s.%N)
 
-    python3 app.py $1&
-    pid=$!
+python3 app.py $1&
+pid=$!
 
-    echo "Resource usage of app.py: " >> results.txt
-    echo "PID USER PR NI VIRT RES SHR S %CPU TIME+ COMMAND" >> results.txt
-    top -b -n1 -p $pid | grep $pid >> results.txt
+echo "Resource usage of app.py: " >> results.txt
+echo "PID USER PR NI VIRT RES SHR S %CPU TIME+ COMMAND" >> results.txt
+top -b -n1 -p $pid | grep $pid >> results.txt
 
-    end_time=$(date +%s.%N)
-    run_time=$(echo "$end_time - $start_time" | bc)
-    execution_time+=($run_time)
-    echo "execution time $i: $run_time seconds" >> results.txt
-done
+end_time=$(date +%s.%N)
+run_time=$(echo "$end_time - $start_time" | bc)
+echo "execution time $i: $run_time seconds" >> results.txt
