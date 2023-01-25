3a214f0d7bc728ab963f7162c31242f17eebdf4f
Rojjanasak61 <34545417+Rojjanasak61@users.noreply.github.com>
2023-01-25 19:35:17 +0700

    add

--- avg.sh
+++ avg.sh
@@ -12,10 +12,10 @@ do
     end_cpu=$(grep 'cpu' /proc/stat)
     end_mem=$(free -m)
     run_time=$(echo "$end_time - $start_time" | bc)
-    printf "Run %d:\n" $i >> 01-$i-mTest.txt
-    printf "Execution time: %.9f seconds\n" $run_time >> result.txt
-    printf "Start CPU | User | Nice | System | Idle | Iowait | Irq | Softirq | Steal | Guest | Guest_nice\n%s\n" "$start_cpu" >> 01-$i-mTest.txt
-    printf "End CPU | User | Nice | System | Idle | Iowait | Irq | Softirq | Steal | Guest | Guest_nice\n%s\n" "$end_cpu" >> 01-$i-mTest.txt
-    printf "Start MEM \n%s\n" "$start_mem" >> 01-$i-mTest.txt
-    printf "End MEM \n%s\n" "$end_mem" >> 01-$i-mTest.txt
+    printf "Run %d:\n" $i >> 01-mTest.txt
+    printf "Execution time: %.9f seconds\n" $run_time >> 01-mTest.txt
+    printf "Start CPU | User | Nice | System | Idle | Iowait | Irq | Softirq | Steal | Guest | Guest_nice\n%s\n" "$start_cpu" >> 01-mTest.txt
+    printf "End CPU | User | Nice | System | Idle | Iowait | Irq | Softirq | Steal | Guest | Guest_nice\n%s\n" "$end_cpu" >> 01-mTest.txt
+    printf "Start MEM \n%s\n" "$start_mem" >> 01-mTest.txt
+    printf "End MEM \n%s\n" "$end_mem" >> 01-mTest.txt
 done
