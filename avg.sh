1373fe207277f5b7803663dfb38a7eb3343077d9
Rojjanasak61 <34545417+Rojjanasak61@users.noreply.github.com>
2023-02-06 18:42:31 +0700

    add

--- avg.sh
+++ avg.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
+filename = "01-mTest"
 for i in {1..10}
 do
     start_time=$(date +%s.%N)
-    start_cpu=$(grep 'cpu' /proc/stat)
-    start_mem=$(free -m)
+    printf "Run %d:\n" $i >> $filename.txt
 
-    ./app.py $1 cpu_ram.sh
+    ./app.py "$1" & ./cpu_ram.sh $filename
 
     end_time=$(date +%s.%N)
-    end_cpu=$(grep 'cpu' /proc/stat)
-    end_mem=$(free -m)
     run_time=$(echo "$end_time - $start_time" | bc)
-    printf "Run %d:\n" $i >> 01-mTest.txt
-    printf "Execution time: %.9f seconds\n" $run_time >> 01-mTest.txt
-    printf "Start CPU | User | Nice | System | Idle | Iowait | Irq | Softirq | Steal | Guest | Guest_nice\n%s\n" "$start_cpu" >> 01-mTest.txt
-    printf "End CPU | User | Nice | System | Idle | Iowait | Irq | Softirq | Steal | Guest | Guest_nice\n%s\n" "$end_cpu" >> 01-mTest.txt
-    printf "Start MEM \n%s\n" "$start_mem" >> 01-mTest.txt
-    printf "End MEM \n%s\n" "$end_mem" >> 01-mTest.txt
+    printf "Execution time: %.9f seconds\n" $run_time >> $filename.txt
 done
--- cpu_ram.sh
+++ cpu_ram.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-for i in {0..10}; do
-    echo "Time: $i seconds">> resource_usage.txt
-    echo "CPU usage: $(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')">> resource_usage.txt
-    echo "RAM usage: $(free | grep Mem | awk '{print $3/$2 * 100.0}')%">> resource_usage.txt
-    sleep 2
-done
+sar 1 1 -P ALL > $1.txt
+sar -r 1 1 -P ALL > $1.txt
