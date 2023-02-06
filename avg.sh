20d8e400ee5b45608dc3b939ae82f6bca2657bab
Rojjanasak61 <34545417+Rojjanasak61@users.noreply.github.com>
2023-02-06 18:58:58 +0700

    add

--- avg.sh
+++ avg.sh
@@ -5,9 +5,10 @@ do
     start_time=$(date +%s.%N)
     printf "Run %d:\n" $i >> $filename.txt
 
-    ./app.py "$1" & ./cpu_ram.sh $filename
+    ./app.py "$1"
 
     end_time=$(date +%s.%N)
     run_time=$(echo "$end_time - $start_time" | bc)
     printf "Execution time: %.9f seconds\n" $run_time >> $filename.txt
+    echo "end loop $1"
 done
